c4db87dc1fad367f44ddf28f1fe9469a718b6d3c
Lin Yang <liny@outlook.com>
2018-07-15 23:12:40 +1000

    basic function

--- canou.lua
+++ canou.lua
@@ -21,7 +21,26 @@ local f_proto = ProtoField.uint8("multi.protocol", "Protocol", base.DEC, vs_prot
 local f_dir = ProtoField.uint8("multi.direction", "Direction", base.DEC, { [1] = "incoming", [0] = "outgoing"})
 local f_text = ProtoField.string("multi.text", "Text")
 
-CANoU.fields = { f_proto, f_dir, f_text }
+local canid_set = {
+        [0x37] = "Acceleration via ICE",
+        [0xb4] = "Current speed of the automobile",
+        [0x1c4] = "ICE RPM",
+        [0x24] = "ICE Torque",
+        [0x224] = "Brake pedal position sensor",
+        [0x25] = "Steer wheel angle",
+        [0x230] = "Brake sensor",
+        [0x245] = "Gas pedal position",
+        [0x283] = "Pre-collision action",
+        [0x7df] = "Universal diagnostic message"
+}
+local f_canid = ProtoField.uint32("canou.canid", "CAN ID", base.DEC, canid_set)
+local f_canlen = ProtoField.uint8("canou.msglen", "Msg Len", base.DEC)
+--local f_canval = ProtoField.uint32("canou.msgval", "Msg Value", base.DEC)
+local f_canmsg = ProtoField.string("canou.text", "Msg Info")
+
+
+--CANoU.fields = { f_proto, f_dir, f_text }
+CANoU.fields = { f_canid, f_canlen, f_canval, f_canmsg }
 
 local data_dis = Dissector.get("data")
 
@@ -42,6 +61,20 @@ local protos = {
 -- dissect packet
 function CANoU.dissector(buf, pkt, tree)
 
+        local subtree = tree:add(CANoU, buf(0,24))
+        subtree:add(f_canid, buf(8,4))
+        subtree:add(f_canlen, buf(15,1))
+--        subtree:add(f_canval, buf(16,8))
+
+        local canid = buf(8,4):uint()
+        local msg_string = canid_set[canid]
+        if msg_string == nil then
+                subtree:add(f_canmsg, "unknown message type")
+        else
+                subtree:add(f_canmsg, msg_string)
+        end
+
+        --[[
         local subtree = tree:add(CANoU, buf(0,2))
         subtree:add(f_proto, buf(0,1))
         subtree:add(f_dir, buf(1,1))
@@ -61,13 +94,14 @@ function CANoU.dissector(buf, pkt, tree)
                 -- fallback dissector that just shows the raw data.
                 data_dis:call(buf(2):tvb(), pkt, tree)
         end
+        --]]
 
 end
 
 -- register this dissector
-local wtap_encap_table = DissectorTable.get("wtap_encap")
+--local wtap_encap_table = DissectorTable.get("wtap_encap")
 local udp_encap_table = DissectorTable.get("udp.port")
 
-wtap_encap_table:add(wtap.USER15, CANoU)
-wtap_encap_table:add(wtap.USER12, CANoU)
+--wtap_encap_table:add(wtap.USER15, CANoU)
+--wtap_encap_table:add(wtap.USER12, CANoU)
 udp_encap_table:add(PORT, CANoU)
