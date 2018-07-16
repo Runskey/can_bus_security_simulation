de218816930e9232604ff103da7f05d66577e742
Lin Yang <liny@outlook.com>
2018-07-16 11:19:55 +1000

    add bit and operation for first byte

--- canou.lua
+++ canou.lua
@@ -7,7 +7,6 @@ local NAME = "CANoU"
 local PORT = 3248
 local CANoU = Proto(NAME, "CAN-over-UDP Protocol")
 
-
 local canid_set = {
         [0x37] = "Acceleration via ICE",
         [0xb4] = "Current speed of the automobile",
@@ -20,6 +19,7 @@ local canid_set = {
         [0x283] = "Pre-collision action",
         [0x7df] = "Universal diagnostic message"
 }
+
 local DBC_DATABASE = {
         [0x0037] = {['bytelen']=7, ['bit_start']= 0, ['bit_num']=40, ['scale']=1.0, ['offset']=0.0},
         [0x00b4] = {['bytelen']=8, ['bit_start']=47, ['bit_num']=16, ['scale']=0.01, ['offset']=0.0},
@@ -33,46 +33,12 @@ local DBC_DATABASE = {
         [0x07df] = {['bytelen']=8, ['bit_start']=15, ['bit_num']=16, ['scale']=1.0, ['offset']=0.0}
 }
 
---[[
-local vs_protos = {
-        [2] = "mtp2",
-        [3] = "mtp3",
-        [4] = "alcap",
-        [5] = "h248",
-        [6] = "ranap",
-        [7] = "rnsap",
-        [8] = "nbap"
-}
+local f_canid = ProtoField.uint32("canou.canid", "CAN Message Name", base.DEC, canid_set)
+local f_canlen = ProtoField.uint8("canou.msglen", "Length of Byte", base.DEC)
+local f_canval = ProtoField.uint32("canou.msgval", "Value", base.DEC)
+local f_canmsg = ProtoField.string("canou.text", "Addi. Info")
 
-local f_proto = ProtoField.uint8("multi.protocol", "Protocol", base.DEC, vs_protos)
-local f_dir = ProtoField.uint8("multi.direction", "Direction", base.DEC, { [1] = "incoming", [0] = "outgoing"})
-local f_text = ProtoField.string("multi.text", "Text")
---]]
-
-local f_canid = ProtoField.uint32("canou.canid", "CAN ID", base.DEC, canid_set)
-local f_canlen = ProtoField.uint8("canou.msglen", "Msg Len", base.DEC)
-local f_canval = ProtoField.uint32("canou.msgval", "Msg Value", base.DEC)
-local f_canmsg = ProtoField.string("canou.text", "Msg Info")
-
---CANoU.fields = { f_proto, f_dir, f_text }
 CANoU.fields = { f_canid, f_canlen, f_canval, f_canmsg }
-
---[[
-local data_dis = Dissector.get("data")
-local protos = {
-        [2] = Dissector.get("mtp2"),
-        [3] = Dissector.get("mtp3"),
-        [4] = Dissector.get("alcap"),
-        [5] = Dissector.get("h248"),
-        [6] = Dissector.get("ranap"),
-        [7] = Dissector.get("rnsap"),
-        [8] = Dissector.get("nbap"),
-        [9] = Dissector.get("rrc"),
-        [10] = DissectorTable.get("sctp.ppi"):get_dissector(3), -- m3ua
-        [11] = DissectorTable.get("ip.proto"):get_dissector(132), -- sctp
-}
---]]
-
 
 -- dissect packet
 function CANoU.dissector(buf, pkt, tree)
@@ -98,49 +64,28 @@ function CANoU.dissector(buf, pkt, tree)
         then
                 value = canid
         else
-                local bit_start = decoder['bit_start']
-                local bit_num = decoder['bit_num']
-                local value_byte_num = (bit_start+bit_num) / 8
-                local bit_shift = 8 - (bit_start+bit_num) % 8
+                local value_byte_num = (decoder['bit_start'] + decoder['bit_num']) / 8
+                local bit_shift = 8 - (decoder['bit_start'] + decoder['bit_num']) % 8
 
                 for i=0,value_byte_num do
                         local newbyte = buf(16+i,1):uint()
+                        -- to remove prefix for first byte
+                        --[[
+                        if i == 0
+                        then
+                                local bitmask = 2 ^ (8 - decoder['bit_start'] % 8)
+                                newbyte = newbyte & bitmask
+                        end
                         value = (value * 256) + newbyte
+                        --]]
                 end
                 value = value / (2^bit_shift)
                 value = value * decoder['scale']+decoder['offset']
         end
 
         subtree:add(f_canval, value)
-
-        --[[
-        local subtree = tree:add(CANoU, buf(0,2))
-        subtree:add(f_proto, buf(0,1))
-        subtree:add(f_dir, buf(1,1))
-
-        local proto_id = buf(0,1):uint()
-
-        local dissector = protos[proto_id]
-
-        if dissector ~= nil then
-                -- Dissector was found, invoke subdissector with a new Tvb,
-                -- created from the current buffer (skipping first two bytes).
-                dissector:call(buf(2):tvb(), pkt, tree)
-        elseif proto_id < 2 then
-                subtree:add(f_text, buf(2))
-                -- pkt.cols.info:set(buf(2, buf:len() - 3):string())
-        else
-                -- fallback dissector that just shows the raw data.
-                data_dis:call(buf(2):tvb(), pkt, tree)
-        end
-        --]]
-
 end
 
 -- register this dissector
---local wtap_encap_table = DissectorTable.get("wtap_encap")
 local udp_encap_table = DissectorTable.get("udp.port")
-
---wtap_encap_table:add(wtap.USER15, CANoU)
---wtap_encap_table:add(wtap.USER12, CANoU)
 udp_encap_table:add(PORT, CANoU)
